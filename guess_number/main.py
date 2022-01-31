5d7e18efe7d29880f59e0cf636ebcbe4610ea730
filipgaldyn <filip.galdyn.fg@gmail.com>
2022-01-31 20:31:45 +0100

    add new proj #1 guess_number

---
+++ guess_number/main.py
@@ -0,0 +1,35 @@
+import random
+
+def guess(x):
+    random_number = random.randint(1, x)
+    guess = 0
+    while guess != random_number:
+        guess = int(input(f"Guess a number between 1 and {x}: "))
+        if guess < random_number:
+            print('Soryy, guess again. Too low.')
+        elif guess > random_number:
+            print('Soryy, guess again. Too high.')
+
+    print(f'Yea, you have guessed the correct number {random_number}!')
+
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ''
+    while feedback != 'c':
+        if low != high:
+            guess = random.randint(low, high)
+        else:
+            guess = low
+
+        feedback = input(f'Is {guess} to high (H), too low (L), or correct (C)??').lower()
+        if feedback == 'h':
+            high = guess - 1
+        if feedback == 'l':
+            low = guess + 1
+
+    print(f"Yay! The computer guessed your number, {guess}, correctly!")
+
+
+guess(6) # <--- here put range of numbers
+computer_guess(6) # <--- here put range of numbers
