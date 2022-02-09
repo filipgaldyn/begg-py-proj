e5c0ebac16581d0455275cb12a72c2eba9e487f2
filipgaldyn <filip.galdyn.fg@gmail.com>
2022-02-09 11:23:19 +0100

    remove api key

--- football-data.py
+++ football-data.py
@@ -3,7 +3,7 @@ import http.client
 import json
 
 connection = http.client.HTTPConnection('api.football-data.org')
-headers = { 'X-Auth-Token': 'c871a127e8ec4446bda54a895759d7f2' }
+headers = { 'X-Auth-Token': 'KEY' }
 connection.request('GET', '/v2/competitions/PL', None, headers )
 response = json.loads(connection.getresponse().read().decode())
 
